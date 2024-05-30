41ea39ac8e8d3d53e1d44eecf76b7f59fce81847
Lolis0 <zunigalolis6@gmail.com>
2024-05-29 18:55:50 -0600

    PRACTICA 5 PARTE 2

--- DML-JoinsComicStore.sql
+++ DML-JoinsComicStore.sql
@@ -1,4 +1,32 @@
 
 
+
+
+
+
+
+
+
+
+
+
 -- Inner Join
-Select c.id_compra, cl.nombre AS CLIENTE
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+JOIN Clientes cl ON c.id_cliente = cl.id_cliente;
+
+-- LEFT Join
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+LEFT JOIN Clientes cl ON c.id_cliente = cl.id_cliente;
+
+-- RIGHT JOIN
+Select c.id_compra, cl.nombre AS CLIENTE, c.fecha_compra, c.total
+from Compras c
+RIGHT JOIN Clientes cl ON c.id_cliente = cl.id_cliente;
+
+--FULL JOIN 
+SELECT * 
+FROM Clientes cl
+FULL JOIN Compras c
+ON c.id_cliente = cl.id_cliente;
